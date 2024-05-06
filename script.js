1921581b053f8f518fbeaca30b36ff72f71ed479
Kubanec01 <156695557+Kubanec01@users.noreply.github.com>
2024-05-06 20:47:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Variables
 var introContainer = document.querySelectorAll('.introducing-container');
 var welcomeContainer = document.querySelector('.welcome');
-var newCategoryValue;
 var moneyIcon = document.querySelector('.bx.bx-euro');
 window.addEventListener('scroll', checkBoxes);
 checkBoxes();
@@ -25,75 +24,139 @@ var foodInput = document.querySelector('.food-input');
 var foodPlusTag = document.querySelector('.food-plus');
 var foodMinusTag = document.querySelector('.food-minus');
 var foodCategory = document.querySelector('.food p');
-if (foodInput && foodPlusTag && foodCategory && moneyIcon) {
-    // Nastavení události kliknutí na tlačítko foodPlusTag
+if (!foodInput || !foodPlusTag || !foodMinusTag || !foodCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+}
+else {
+    var newFoodCategoryValue_1;
     foodPlusTag.addEventListener('click', function () {
-        // Získání hodnoty z foodInput a převedení na číslo
         var inputValue = parseFloat(foodInput.value);
-        // Získání aktuální hodnoty textu v foodCategory, odstranění měnové ikony a převedení na číslo
-        var currentCategoryValue = parseFloat(foodCategory.textContent.replace(/\D/g, ''));
-        // Pokud je vstupní hodnota platná, přičte se k aktuální hodnotě kategorie
-        if (!isNaN(inputValue)) {
-            newCategoryValue = inputValue + currentCategoryValue;
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
         }
-        // Pokud je nová hodnota NaN (neplatná), neaktualizuje se nic
-        if (!isNaN(newCategoryValue)) {
-            // Aktualizace textu v foodCategory s novou číselnou hodnotou a přidáním ikony měny
-            foodCategory.textContent = "".concat(newCategoryValue, " ");
-            foodCategory.appendChild(moneyIcon);
-        }
+        updateFoodCategory_1(inputValue);
     });
-}
-else {
-    console.error('Některý z prvků nebyl nalezen.');
-}
-if (foodInput && foodMinusTag && foodCategory && moneyIcon) {
     foodMinusTag.addEventListener('click', function () {
-        var inputValue = parseInt(foodInput.value);
-        var currentCategoryValue = parseFloat(foodCategory.textContent.replace(/\D/g, ''));
-        if (!isNaN(inputValue)) {
-            newCategoryValue = currentCategoryValue - inputValue;
-        }
-        if (!isNaN(newCategoryValue)) {
-            foodCategory.textContent = "".concat(newCategoryValue);
-            foodCategory.appendChild(moneyIcon);
+        var inputValue = parseFloat(foodInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFoodCategory_1(-inputValue);
         }
     });
-}
-else {
-    console.log("Error: Invalid input value");
+    var updateFoodCategory_1 = function (value) {
+        var currentCategoryValue = parseFloat(foodCategory.textContent.replace(/\D/g, ''));
+        newFoodCategoryValue_1 = currentCategoryValue + value;
+        if (newFoodCategoryValue_1 >= 0) {
+            foodCategory.textContent = "".concat(newFoodCategoryValue_1);
+            var newMoneyIcon = moneyIcon.cloneNode(true);
+            foodCategory.appendChild(newMoneyIcon);
+        }
+        else {
+            console.error('Category cannot be negative.');
+        }
+    };
 }
 // Home Input Function
 var homeInput = document.querySelector('.home-input');
 var homePlusTag = document.querySelector('.home-plus');
 var homeMinusTag = document.querySelector('.home-minus');
 var homeCategory = document.querySelector('.home p');
-if (homeInput && homePlusTag && homeCategory && moneyIcon) {
-    homePlusTag.addEventListener('click', function () {
-        var inputValue = parseFloat(homeInput.value);
-        var currentCategoryValue = parseFloat(homeCategory.textContent.replace(/\D/g, ''));
-        if (!isNaN(inputValue)) {
-            newCategoryValue = inputValue + currentCategoryValue;
-        }
-        if (!isNaN(newCategoryValue)) {
-            homeCategory.textContent = "".concat(newCategoryValue);
-            homeCategory.appendChild(moneyIcon);
-        }
-    });
+if (!homeInput || !homePlusTag || !homeMinusTag || !homeCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
 }
 else {
-    console.log("Error: Invalid input value");
-}
-if (homeInput && homeMinusTag && homeCategory && moneyIcon) {
+    var newHomeCategoryValue_1;
+    homePlusTag.addEventListener('click', function () {
+        var inputValue = parseFloat(homeInput.value);
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateHomeCategory_1(inputValue);
+    });
     homeMinusTag.addEventListener('click', function () {
         var inputValue = parseFloat(homeInput.value);
-        var currentCategoryValue = parseFloat(homeCategory.textContent.replace(/\D/g, ''));
-        if (!isNaN(inputValue)) {
-            newCategoryValue = currentCategoryValue - inputValue;
-        }
-        if (!isNaN(currentCategoryValue)) {
-            homeCategory.textContent = "".concat(newCategoryValue);
-            homeCategory.appendChild(moneyIcon);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateHomeCategory_1(-inputValue);
         }
     });
+    var updateHomeCategory_1 = function (value) {
+        var currentCategoryValue = parseFloat(homeCategory.textContent.replace(/\D/g, ''));
+        newHomeCategoryValue_1 = currentCategoryValue + value;
+        if (newHomeCategoryValue_1 >= 0) {
+            homeCategory.textContent = "".concat(newHomeCategoryValue_1);
+            var newMoneyIcon = moneyIcon.cloneNode(true);
+            homeCategory.appendChild(newMoneyIcon);
+        }
+    };
 }
+// Fuel Input Function
+var fuelInput = document.querySelector('.fuel-input');
+var fuelPlusTag = document.querySelector('.fuel-plus');
+var fuelMinusTag = document.querySelector('.fuel-minus');
+var fuelCategory = document.querySelector('.fuel p');
+if (!fuelInput || !fuelPlusTag || !fuelMinusTag || !fuelCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+}
+else {
+    var newFuelCategoryValue_1;
+    fuelPlusTag.addEventListener('click', function () {
+        var inputValue = parseFloat(fuelInput.value);
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateFuelCategory_1(inputValue);
+    });
+    fuelMinusTag.addEventListener('click', function () {
+        var inputValue = parseFloat(fuelInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFuelCategory_1(-inputValue);
+        }
+    });
+    var updateFuelCategory_1 = function (value) {
+        var currentCategoryValue = parseFloat(fuelCategory.textContent.replace(/\D/g, ''));
+        newFuelCategoryValue_1 = currentCategoryValue + value;
+        if (newFuelCategoryValue_1 >= 0) {
+            fuelCategory.textContent = "".concat(newFuelCategoryValue_1);
+            var newMoneyIcon = moneyIcon.cloneNode(true);
+            fuelCategory.appendChild(newMoneyIcon);
+        }
+    };
+}
+// Fun Input Function
+var funInput = document.querySelector('.fun-input');
+var funPlusTag = document.querySelector('.fun-plus');
+var funMinusTag = document.querySelector('.fun-minus');
+var funCategory = document.querySelector('.fun p');
+if (!funInput || !funPlusTag || !funMinusTag || !funCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+}
+else {
+    var newFunCategoryValue_1;
+    funPlusTag.addEventListener('click', function () {
+        var inputValue = parseFloat(funInput.value);
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateFunCategory_1(inputValue);
+    });
+    funMinusTag.addEventListener('click', function () {
+        var inputValue = parseFloat(funInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFunCategory_1(-inputValue);
+        }
+    });
+    var updateFunCategory_1 = function (value) {
+        var currentCategoryValue = parseFloat(funCategory.textContent.replace(/\D/g, ''));
+        newFunCategoryValue_1 = currentCategoryValue + value;
+        if (newFunCategoryValue_1 >= 0) {
+            funCategory.textContent = "".concat(newFunCategoryValue_1);
+            var newMoneyIcon = moneyIcon.cloneNode(true);
+            funCategory.appendChild(newMoneyIcon);
+        }
+    };
+}
+// Other Input Function
+var otherInput = document.querySelector('.other-input');
+var otherPlusTag = document.querySelector('.other-plus');
+var otherMinusTag = document.querySelector('.other-minus');
+var otherCategory = document.querySelector('.other p');
+// Sem dopln funkciu na vypocet Other kategorie
--- script.ts
+++ script.ts
@@ -1,7 +1,6 @@
 // Variables
 const introContainer = document.querySelectorAll('.introducing-container');
 const welcomeContainer = document.querySelector('.welcome')
-let newCategoryValue: number;
 const moneyIcon = document.querySelector('.bx.bx-euro');
 
 
@@ -35,49 +34,37 @@ const foodPlusTag = document.querySelector('.food-plus');
 const foodMinusTag = document.querySelector('.food-minus');
 const foodCategory = document.querySelector('.food p');
 
-if (foodInput && foodPlusTag && foodCategory && moneyIcon) {
-    // Nastavení události kliknutí na tlačítko foodPlusTag
-    foodPlusTag.addEventListener('click', () => {
-        // Získání hodnoty z foodInput a převedení na číslo
-        const inputValue = parseFloat(foodInput.value);
-
-        // Získání aktuální hodnoty textu v foodCategory, odstranění měnové ikony a převedení na číslo
-        const currentCategoryValue = parseFloat(foodCategory.textContent!.replace(/\D/g, ''));
-
-        // Pokud je vstupní hodnota platná, přičte se k aktuální hodnotě kategorie
-        if (!isNaN(inputValue)) {
-            newCategoryValue = inputValue + currentCategoryValue;
-        }
-
-        // Pokud je nová hodnota NaN (neplatná), neaktualizuje se nic
-        if (!isNaN(newCategoryValue)) {
-            // Aktualizace textu v foodCategory s novou číselnou hodnotou a přidáním ikony měny
-            foodCategory.textContent = `${newCategoryValue} `;
-            foodCategory.appendChild(moneyIcon);
-        }
-    });
+if (!foodInput || !foodPlusTag || !foodMinusTag || !foodCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
 } else {
-    console.error('Některý z prvků nebyl nalezen.');
-}
+    let newFoodCategoryValue: number;
 
-if (foodInput && foodMinusTag && foodCategory && moneyIcon) {
+    foodPlusTag.addEventListener('click', () => {
+        const inputValue = parseFloat(foodInput.value);
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateFoodCategory(inputValue);
+    });
 
     foodMinusTag.addEventListener('click', () => {
-        const inputValue = parseInt(foodInput.value);
-
-        const currentCategoryValue = parseFloat(foodCategory.textContent!.replace(/\D/g, ''));
-
-        if (!isNaN(inputValue)) {
-            newCategoryValue = currentCategoryValue - inputValue;
+        const inputValue = parseFloat(foodInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFoodCategory(-inputValue);
         }
-        
-        if (!isNaN(newCategoryValue)) {
-            foodCategory.textContent = `${newCategoryValue}`
-            foodCategory.appendChild(moneyIcon);
+    });
+
+    const updateFoodCategory = (value: number) => {
+        const currentCategoryValue = parseFloat(foodCategory!.textContent!.replace(/\D/g, ''));
+        newFoodCategoryValue = currentCategoryValue + value;
+        if (newFoodCategoryValue >= 0) {
+            foodCategory!.textContent = `${newFoodCategoryValue}`;
+            const newMoneyIcon = moneyIcon!.cloneNode(true) as HTMLElement;
+            foodCategory!.appendChild(newMoneyIcon);
+        } else {
+            console.error('Category cannot be negative.');
         }
-    })
-} else {
-    console.log("Error: Invalid input value")
+    }
 }
 
 
@@ -87,49 +74,121 @@ const homePlusTag = document.querySelector('.home-plus');
 const homeMinusTag = document.querySelector('.home-minus');
 const homeCategory = document.querySelector('.home p');
 
-if (homeInput && homePlusTag && homeCategory && moneyIcon) {
+if (!homeInput || !homePlusTag || !homeMinusTag || !homeCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+} else {
+    let newHomeCategoryValue: number;
 
     homePlusTag.addEventListener('click', () => {
         const inputValue = parseFloat(homeInput.value);
-
-        const currentCategoryValue = parseFloat(homeCategory.textContent!.replace(/\D/g, ''));
-
-        if (!isNaN(inputValue)) {
-            newCategoryValue = inputValue + currentCategoryValue;
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
         }
-
-        if(!isNaN(newCategoryValue)) {
-            homeCategory.textContent = `${newCategoryValue}`
-            homeCategory.appendChild(moneyIcon)
-        }
-    })
-} else {
-    console.log("Error: Invalid input value");
-}
-
-if (homeInput && homeMinusTag && homeCategory && moneyIcon) {
+        updateHomeCategory(inputValue);
+    });
 
     homeMinusTag.addEventListener('click', () => {
         const inputValue = parseFloat(homeInput.value);
-        
-        const currentCategoryValue = parseFloat(homeCategory.textContent!.replace(/\D/g, ''));
-
-        if (!isNaN(inputValue)) {
-            newCategoryValue = currentCategoryValue - inputValue;
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateHomeCategory(-inputValue);
         }
+    });
 
-        if(!isNaN(currentCategoryValue)) {
-            homeCategory.textContent = `${newCategoryValue}`
-            homeCategory.appendChild(moneyIcon)
+    const updateHomeCategory = (value: number) => {
+        const currentCategoryValue = parseFloat(homeCategory!.textContent!.replace(/\D/g, ''));
+        newHomeCategoryValue = currentCategoryValue + value;
+        if (newHomeCategoryValue >= 0) {
+            homeCategory!.textContent = `${newHomeCategoryValue}`;
+            const newMoneyIcon = moneyIcon!.cloneNode(true) as HTMLElement;
+            homeCategory.appendChild(newMoneyIcon);
         }
-    })
-} else {
-    console.log("Error: Invalid input value");
+    } 
 }
 
-// Fuel Input Function
 
+// Fuel Input Function
 const fuelInput = document.querySelector('.fuel-input') as HTMLInputElement;
 const fuelPlusTag = document.querySelector('.fuel-plus');
 const fuelMinusTag = document.querySelector('.fuel-minus');
 const fuelCategory = document.querySelector('.fuel p');
+
+if (!fuelInput || !fuelPlusTag || !fuelMinusTag || !fuelCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+} else {
+    let newFuelCategoryValue: number;
+
+    fuelPlusTag.addEventListener('click', () => {
+        const inputValue = parseFloat(fuelInput.value)
+        if (isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateFuelCategory(inputValue);
+    });
+
+    fuelMinusTag.addEventListener('click', () => {
+        const inputValue = parseFloat(fuelInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFuelCategory(-inputValue);
+        }
+    });
+
+    const updateFuelCategory = (value :number) => {
+        const currentCategoryValue = parseFloat(fuelCategory!.textContent!.replace(/\D/g, ''));
+        newFuelCategoryValue = currentCategoryValue + value;
+        if (newFuelCategoryValue >= 0) {
+            fuelCategory!.textContent = `${newFuelCategoryValue}`;
+            const newMoneyIcon = moneyIcon!.cloneNode(true) as HTMLElement;
+            fuelCategory.appendChild(newMoneyIcon);
+        }
+    }
+}
+
+
+
+// Fun Input Function
+const funInput = document.querySelector('.fun-input') as HTMLInputElement;
+const funPlusTag = document.querySelector('.fun-plus');
+const funMinusTag = document.querySelector('.fun-minus');
+const funCategory = document.querySelector('.fun p');
+
+if (!funInput || !funPlusTag || !funMinusTag || !funCategory || !moneyIcon) {
+    console.error('One of the elements was not found.');
+} else {
+    let newFunCategoryValue: number;
+
+    funPlusTag.addEventListener('click', () => {
+        const inputValue = parseFloat(funInput.value);
+        if(isNaN(inputValue)) {
+            console.error('Invalid operation: Input value must be greater than zero.');
+        }
+        updateFunCategory(inputValue);
+    });
+
+    funMinusTag.addEventListener('click', () => {
+        const inputValue = parseFloat(funInput.value);
+        if (!isNaN(inputValue) && inputValue > 0) {
+            updateFunCategory(-inputValue);
+        }
+    });
+
+    const updateFunCategory = (value: number) => {
+        const currentCategoryValue = parseFloat(funCategory!.textContent!.replace(/\D/g, ''));
+        newFunCategoryValue = currentCategoryValue + value;
+
+        if (newFunCategoryValue >= 0) {
+            funCategory!.textContent = `${newFunCategoryValue}`;
+            const newMoneyIcon = moneyIcon!.cloneNode(true) as HTMLElement;
+            funCategory.appendChild(newMoneyIcon);
+        }
+    }
+
+}
+
+
+// Other Input Function
+const otherInput = document.querySelector('.other-input') as HTMLInputElement;
+const otherPlusTag = document.querySelector('.other-plus');
+const otherMinusTag = document.querySelector('.other-minus');
+const otherCategory = document.querySelector('.other p');
+
+// Sem dopln funkciu na vypocet Other kategorie
